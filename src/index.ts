d74495f29b638739a0a29b0535974670abf982c8
n3taway <n3taway@gmail.com>
2023-12-13 18:51:02 +0800

    feat: 移除特殊字符，转为小写翻译

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,15 @@ const main = async () => {
 
   const word: string = Array.from(tjs.args).pop() as string;
 
-  const result = await translator.translate(word);
+  // case
+  // YOUDAO_TRANSLATOR 英语
+  // TELÉFONO 西班牙语言
+
+  // 移除特殊字符，转为小写翻译
+
+  // /Users/<username>/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows
+
+  const result = await translator.translate(word.replace(/_/g, ' ').toLocaleLowerCase());
 
   console.log(result);
 }
